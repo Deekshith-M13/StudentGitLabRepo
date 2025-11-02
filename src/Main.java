4da32a916be7ec5cada422cb644a9412f2ce8c32
Deekshith M <deekshith.m2005@gmail.com>
2025-11-02 10:32:24 +0530

    Modified the Main.java file - this is version 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Git! This is my first Version Control Project");
+        System.out.println("Hello Git! This is my Second Version Control Project");
+        System.out.println("How Cool is it!");
     }
 }
